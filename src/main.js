e5374724d1b37ecc69aaa805c737a945d833c208
u.tchatong <u.tchatong@itgstore-consulting.com>
2021-04-23 18:45:53 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+import './assets/js/core/jquery.min.js'
+import './assets/js/core/popper.min.js'
+//import './assets/js/core/bootstrap-material-design.min.js'
+//import './assets/js/plugins/perfect-scrollbar.jquery.min.js'
+//import './assets/js/material-dashboard.js?v=2.1.2'
+import './assets/demo/demo.js'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+  export  default {
+    created() {
+      console.log('Je suis crée !')
+    },
+    mounted() {
+      console.log('Je suis monté !')
+    },
+    beforeDestroy() {
+      console.log('Je vais être détruit !')
+    },
+    beforeCreate() {
+      console.log('Je vais être crée !')
+    },
+    destroyed() {
+      console.log('Je suis détruit !')
+    },
+    beforeMount() {
+      console.log('Je vais être monté !')
+    }
+  }
+</script>
+
+<style>
+</style>
