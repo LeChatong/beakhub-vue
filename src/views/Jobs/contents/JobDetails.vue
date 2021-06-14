6e34ea21684bb3981a55d750048edbdd3afdc631
u.tchatong <u.tchatong@itgstore-consulting.com>
2021-06-14 18:53:13 +0100

    Add navigation

--- src/views/Jobs/contents/JobDetails.vue
+++ src/views/Jobs/contents/JobDetails.vue
@@ -5,7 +5,7 @@
                 <h3 class="mb-0">Details Job</h3>
             </b-col>
             <b-col cols="4" class="text-right">
-                <a @click="emitJobDetailsEvent" href="javascript:;" class="btn btn-sm" v-bind:class="{'btn-danger' : job.is_active}">
+                <a @click="emitJobDetailsEvent" href="javascript:;" class="btn btn-sm">
                     activate
                 </a>
                 <a @click="changeEditionStaus" href="javascript:;" class="btn btn-sm btn-primary">
@@ -33,7 +33,6 @@
                                 label="User"
                                 placeholder="johndoe"
                                 :disabled="!edit"
-                                v-model="job.user.username"
                         >
                         </base-input>
                     </b-col>
@@ -43,14 +42,13 @@
                         <base-input
                                 type="text"
                                 label="Category"
-                                :disabled="!edit"
-                                v-model="job.category"/>
+                                :disabled="!edit"/>
                     </b-col>
                     <b-col lg="6">
                         <label class="form-control-label">Status</label>
                         <div>
-                            <a v-show="job.is_active" href="javascript:;" class="btn btn-sm btn-success">Active</a>
-                            <a v-show="!job.is_active" href="javascript:;" class="btn btn-sm btn-error">Inactive</a>
+                            <a href="javascript:;" class="btn btn-sm btn-success">Active</a>
+                            <a href="javascript:;" class="btn btn-sm btn-error">Inactive</a>
                         </div>
                     </b-col>
                 </b-row>
@@ -74,8 +72,7 @@
                                 type="text"
                                 label="Facebook Link"
                                 :disabled="!edit"
-                                placeholder="https://facebook.com/example"
-                                v-model="job.link_facebook"/>
+                                placeholder="https://facebook.com/example"/>
                     </b-col>
                 </b-row>
                 <b-row>
@@ -84,8 +81,7 @@
                                 type="text"
                                 label="Intagram Link"
                                 :disabled="!edit"
-                                placeholder="https://instagram.com/example"
-                                v-model="job.link_instagram"/>
+                                placeholder="https://instagram.com/example"/>
                     </b-col>
                 </b-row>
                 <b-row>
@@ -94,8 +90,7 @@
                                 type="text"
                                 label="Linkedin Link"
                                 :disabled="!edit"
-                                placeholder="https://linkedin.com/example"
-                                v-model="job.link_linkedin"/>
+                                placeholder="https://linkedin.com/example"/>
                     </b-col>
                 </b-row>
                 <b-row>
@@ -104,44 +99,28 @@
                                 type="text"
                                 label="Telegram Link"
                                 :disabled="!edit"
-                                placeholder="https://telegram.com/example"
-                                v-model="job.link_telegram"/>
+                                placeholder="https://telegram.com/example"/>
                     </b-col>
                 </b-row>
             </div>
+
+
         </b-form>
+
     </card>
 </template>
 
 <script>
+
+    import axios from 'axios'
     export default {
         name: "job-details",
         data() {
             return {
-                job: {
-                    title: 'Creative Code Inc.',
-                    description: 'LeChatong',
-                    user: {
-                        company: 'Creative Code Inc.',
-                        username: 'LeChatong',
-                        email: 'ulrich.tchatong@gmail.com',
-                        firstName: 'Tchatong',
-                        lastName: 'Ulrich',
-                        address: 'Logbessou',
-                        city: 'Douala',
-                        country: 'Cameroun',
-                        postalCode: '',
-                        aboutMe: `Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.`
-                    },
-                    category: 'Tchatong',
-                    is_active: true,
-                    link_facebook: 'Logbessou',
-                    link_instagram: 'Douala',
-                    link_linkedin: 'Cameroun',
-                    link_snapchat: '',
-                    link_telegram: `Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.`,
-                    created_at: ''
-                },
+                info: null,
+                loading: true,
+                errored: false,
+                job: [],
                 edit: false
             };
         },
@@ -150,6 +129,7 @@
                 this.$emit('job-detail-event', { typecontent: 'details' })
             },
             changeEditionStaus(){
+                this.fetchJob();
                 if (this.edit){
                     this.edit = false
                 }else{
@@ -157,6 +137,36 @@
                 }
 
                 console.log(this.edit)
+            },
+            async fetchJob() {
+                console.log("LECHATON")
+                let headers = new Headers();
+
+                headers.append('Content-Type', 'application/json');
+                headers.append('Accept', 'application/json');
+
+                headers.append('Access-Control-Allow-Origin', '*');
+
+                try {
+                    const url = `http://localhost:8000/api/v1/job/1/`
+                    const response = await axios.get(url, {headers: headers})
+                    const result = response.data
+                    this.job = result.map(resp => ({
+                        title: resp.title,
+                        description: resp.description
+                    }))
+                } catch (err) {
+                    if (err.response) {
+                        console.log("Server Error:", err)
+                    } else if (err.request) {
+                        console.log("Network Error:", err)
+                    } else {
+                        console.log("Client Error:", err)
+                    }
+                }
+            },
+            mounted() {
+                this.fetchJob();
             }
         }
     }
